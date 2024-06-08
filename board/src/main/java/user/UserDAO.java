faeec6c4e98f4e72c0f4406d79ab669f1e70bc17
Cho-Byeong-Gyu <160321523+Cho-Byeong-Gyu@users.noreply.github.com>
2024-06-08 23:38:08 +0900

    Update UserDAO.java

--- board/src/main/java/user/UserDAO.java
+++ board/src/main/java/user/UserDAO.java
@@ -13,10 +13,10 @@ public class UserDAO {
 	
 	public UserDAO() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:3306/user";
+			String dbURL = "jdbc:mysql://localhost:3306/bbs";
 			String dbID = "root";
-			String dbPW = "4922677";
-			Class.forName("com.mysql.jdbc.Driver");
+			String dbPW = "1234";
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPW);
 			
 		}catch(Exception e){
@@ -34,7 +34,6 @@ public class UserDAO {
       
             if (rs.next()) {
             	if (rs.getString(1).equals(formPW)) {
- 
             		return 1;
             	}
             	else {
@@ -54,15 +53,38 @@ public class UserDAO {
     		pstmt = conn.prepareStatement(SQL);
     		pstmt.setString(1, user.getID());
     		pstmt.setString(2, user.getPW());
-    		pstmt.setString(3, user.getName());
-    		pstmt.setString(4, user.getAge());
-    		pstmt.setString(5, user.getGender());
-    		pstmt.setString(6, user.getIntroduction());
+    		pstmt.setString(3, user.getname());
+    		pstmt.setString(4, user.getage());
+    		pstmt.setString(5, user.getgender());
+    		pstmt.setString(6, user.getintroduction());
     		return pstmt.executeUpdate();
     	} catch(Exception e) {
     		e.printStackTrace();
     	}
     	return -1;
+    }
+    
+    
+    public User getMyInfo (String userID) {
+        String SQL = "SELECT * FROM USER WHERE ID = ?";
+        try {
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, userID);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                User user = new User();
+                user.setID(rs.getString("ID"));
+                user.setPW(rs.getString("PW")); 
+                user.setname(rs.getString("Name"));
+                user.setage(rs.getString("Age"));
+                user.setgender(rs.getString("Gender"));
+                user.setIntroduction(rs.getString("Introduction"));
+                return user;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null; 
     }
     
 }
